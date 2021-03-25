60966f3d776e45f1525ee28921c82e6ac1b5171d
Rodrigo <h2bits@gmail.com>
2021-03-25 14:29:21 -0300

    Dependency Injection using Spring Framework: Property injection.

--- src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,13 +1,17 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 /*
  * Property injection is the least preferred method of DI. Avoid it!
  * As a private property is a problem for testing, we end up using a public one.
  */
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting() {
