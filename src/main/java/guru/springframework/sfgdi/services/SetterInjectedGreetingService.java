93b2007028fb66df9cd45280deef498814c320b3
Rodrigo <h2bits@gmail.com>
2021-03-26 12:32:36 -0300

    Using Qualifiers: Using the @Qualifier annotation to give Spring a hint of what dependency to inject, when there is more than one implementation of an interface.

--- src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
@@ -3,9 +3,8 @@ package guru.springframework.sfgdi.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GreetingServiceImpl implements GreetingService {
-    @Override
+public class SetterInjectedGreetingService implements GreetingService {
     public String sayGreeting() {
-        return "Hello World";
+        return "Hello World - Setter";
     }
 }
